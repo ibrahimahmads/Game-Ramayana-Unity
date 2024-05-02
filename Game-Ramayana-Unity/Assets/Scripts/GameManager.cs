f19b81c197995cc00170a71571da11c1bc40e6b6
Ibrahim Ahmad <ibrahimahmad593@gmail.com>
2024-05-03 02:39:48 +0700

    Menambahkan Game Manager, Serta Scene Main Menu dan Map Chapter, Serta menambahkan Tombol Play,Setting, dan Exit pada Main Menu

---
+++ Game-Ramayana-Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);   
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
