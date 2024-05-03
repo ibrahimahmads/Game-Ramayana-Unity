eb861df5537fe01abfc6d76e862f033046cf0bad
Ibrahim Ahmad <ibrahimahmad593@gmail.com>
2024-05-03 23:34:20 +0700

    menambahkan tombol pindah chapter pada map chapter

---
+++ Game-Ramayana-Unity/Assets/Scripts/MapChapter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapChapter : MonoBehaviour
+{
+    public void pindahChapter1()
+    {
+        SceneManager.LoadScene("Chapter1");
+    } 
+}
